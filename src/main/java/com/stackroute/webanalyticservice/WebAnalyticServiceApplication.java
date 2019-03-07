0f2d20e277ee38f793bd08ca531e10f3aabce62a
venkatanagasairam.sakhamuri <venkatanagasairam.sakhamuri@boeing.com>
2019-03-07 09:27:38 +0530

    Initial Commit

---
+++ src/main/java/com/stackroute/webanalyticservice/WebAnalyticServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.webanalyticservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAnalyticServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAnalyticServiceApplication.class, args);
+	}
+
+}
